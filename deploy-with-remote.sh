122efdcac39555a704ca1ed2c6af921af95b49db
isucon <isucon.local>
2022-07-17 04:38:27 +0000

    init

---
+++ deploy-with-remote.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+
+repo_path=/home/isucon/isucon10-qualify
+branch=${1:-main} # default: main
+
+echo 'running deploy to this server'
+git -C ${repo_path} pull origin main
+git -C ${repo_path} switch ${branch}
+${repo_path}/deploy.sh
+
+echo 'running deploy to other servers, isucon12-02 and isucon12-03'
+echo ''
+echo '[NOTE] Before expecting this process to work well, you need to prepare /etc/hosts for alias isucon12-02, isucon12-03'
+echo ''
+for srv in "isucon12-02" "isucon12-03"
+do
+  echo 'starting for '${srv}
+  ssh isucon@${srv} git -C ${repo_path} pull origin ${branch} || ssh isucon@${srv} git clone https://github.com/t2-isucon/isucon10-qualify.git
+  ssh isucon@${srv} git -C ${repo_path} switch ${branch}
+  ssh isucon@${srv} ${repo_path}/deploy.sh
+  echo 'finished for '${srv}
+done
---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# source $HOME/.bash_profile $HOME/.bashrc
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo 'Copy sql file...'
+sudo cp -r "$DIR/webapp/myql/db" /home/isucon/webapp/mysql/db
+echo 'Copied sql file!'
+
+echo 'Updating config file...'
+# env
+sudo cp "$DIR/env.sh" /home/isucon/env.sh
+
+# nginx
+sudo cp "$DIR/nginx/nginx.conf" /etc/nginx/nginx.conf
+sudo cp "$DIR/nginx/isuumo.conf" /etc/nginx/sites-enabled/isuumo.conf
+
+# mysql
+sudo cp "$DIR/mysql/my.cnf" /etc/mysql/conf.d/my.cnf
+sudo cp "$DIR/mysql/mysqld.cnf" /etc/mysql/mysql.conf.d/mysqld.cnf
+echo 'Updated config file!'
+
+echo 'Restarting Go...'
+sudo systemctl stop isuumo.go.service
+cd $DIR/webapp/go/
+/home/isucon/local/go/bin/go build -o isuumo
+cp isucholar /home/isucon/webapp/go/
+cd $DIR
+sudo systemctl restart isuumo.go.service
+echo 'Restarted!'
+
+echo 'Restarting services...'
+sudo systemctl restart mysql.service
+sudo systemctl restart nginx.service
+echo 'Restarted!'
+
+echo 'Rotating files'
+sudo bash -c 'cp /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%s) && echo > /var/log/nginx/access.log'
+sudo -u mysql bash -c 'test -f /var/log/mysql-slow.log && cp /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.$(date +%s) && echo > /tmp/mysql-slow.sql'
+echo 'Rotated!'
